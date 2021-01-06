eb8ec59ee6d7e49bfd6c55569422ad96db010c40
raquel197 <rgb64@gcloud.ua.es>
2021-01-06 12:50:54 +0100

    guardar

---
+++ guardar/NSave.cpp
@@ -0,0 +1,65 @@
+#include "NSave.hpp"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
+void NSave::escribir(const std::vector< std::vector< std::vector<double> > > v,const std::string s) const{
+
+    std::ofstream ficheroSalida;
+    ficheroSalida.open (s);
+    ficheroSalida << "[";
+    for(int i=0;i<v.size();i++){
+        ficheroSalida << "[";
+        for(int j=0;j<v[i].size();j++){
+            ficheroSalida << "[";
+            for(int k=0;k<v[i][j].size();k++){
+                ficheroSalida << v[i][j][k];
+
+                if(v[i][j].size()-1>k)
+                    ficheroSalida <<" ";
+            }
+            ficheroSalida << "]";
+        }
+        ficheroSalida << "]";
+    }
+    ficheroSalida << "]";
+    
+    ficheroSalida.close();
+
+}
+
+std::vector< std::vector< std::vector<double> > > NSave::leer(const std::string fichero) const{
+    std::vector<  std::vector<  std::vector<double> > > x;
+    std::vector<  std::vector<double> > y;
+    std::vector<double> z;
+    std::string s;
+
+    std::ifstream ficheroEntrada;
+    char letra;
+    ficheroEntrada.open (fichero);
+    ficheroEntrada >> letra;
+    while (! ficheroEntrada.eof() ) {
+        ficheroEntrada >> letra;
+        while(letra!=']'){
+            while(letra!=']'){
+                ficheroEntrada >> letra;
+                if(letra!=' ' && letra!=']' && letra!='['){
+                    s=letra;
+                    z.push_back(atof(s.c_str()));
+                }
+            }
+            y.push_back(z);
+            z.clear();
+            ficheroEntrada >> letra;
+        }
+        if(y.size()>0){
+            x.push_back(y);
+            y.clear();
+        }
+    }
+    ficheroEntrada.close();
+
+    return x;
+}
